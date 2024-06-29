5566a7be176892bbbdb9189823458bc5ea5cdf03
Roman <mac@Mac-my-Roman.local>
2024-06-29 12:23:10 +0300

    fix preloader

--- src/modules/preloader.js
+++ src/modules/preloader.js
@@ -1,10 +1,19 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  window.onload = function () {
-    let preloader = document.getElementById("preloader");
-    preloader.style.opacity = "0";
-    preloader.style.visibility = "hidden";
-    preloader.addEventListener("transitionend", function () {
-      this.remove(); // Это удалит элемент из DOM после завершения анимации исчезновения
-    });
-  };
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   window.onload = function () {
+//     let preloader = document.getElementById("preloader");
+//     preloader.style.opacity = "0";
+//     preloader.style.visibility = "hidden";
+//     preloader.addEventListener("transitionend", function () {
+//       this.remove();
+//     });
+//   };
+// });
+
+window.addEventListener("load", (event) => {
+  let preloader = document.getElementById("preloader");
+  preloader.style.opacity = "0";
+  preloader.style.visibility = "hidden";
+  preloader.addEventListener("transitionend", function () {
+    this.remove();
+  });
 });
